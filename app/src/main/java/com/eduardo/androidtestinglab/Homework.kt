078624f7c0c1a4c3667ef6a630a1f499be44ce14
Eduardo Barron <st.buendia@gmail.com>
2021-10-25 23:15:54 -0500

    fix homework
    fix the fibonacci function in homework

--- app/src/main/java/com/eduardo/androidtestinglab/Homework.kt
+++ app/src/main/java/com/eduardo/androidtestinglab/Homework.kt
@@ -9,19 +9,27 @@ object Homework {
      * fib(1) = 1
      * fib(n) = fib(n-2) + fib(n-1)
      */
+//    fun fib(n: Int): Long {
+//        if(n == 0 || n == 1) {
+//            return n.toLong()
+//        }
+//        var a = 0L
+//        var b = 1L
+//        var c = 1L
+//        (1..n-2).forEach { i ->
+//            c = a + b
+//            a = b
+//            b = c
+//        }
+//        return c
+//    }
+
     fun fib(n: Int): Long {
-        if(n == 0 || n == 1) {
-            return n.toLong()
+        return if(n == 0 || n == 1) {
+            n.toLong()
+        }else{
+            fib(n-2) + fib(n-1)
         }
-        var a = 0L
-        var b = 1L
-        var c = 1L
-        (1..n-2).forEach { i ->
-            c = a + b
-            a = b
-            b = c
-        }
-        return c
     }
 
     /**
